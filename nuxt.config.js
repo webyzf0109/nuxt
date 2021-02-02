61f65350db1f84c5cdd89c7345639f9ba0a74fb8
webyzf0109@163.com <webyzf0109@163.com>
2021-02-02 12:00:20 +0800

    nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,9 +69,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-  router:{
-    base:'/nuxt'
-  },
+  // router:{
+  //   base:'/nuxt'
+  // },
   generate:{
       dir:'docs',
       subFolders:false,
